31ac8d050b0b6f00e933ccedc5b9009d305a40c0
thinkholic <mailtokasun@gmail.com>
2019-12-19 17:24:53 +0530

    Download image

--- scripts/common.js
+++ scripts/common.js
@@ -13,6 +13,7 @@ function triggerTextAreaChange() {
     setTimeout(function() {
         var str = $( '#textbox' ).val();
         setLocalStorage(str);
+        draw();
     }, 0)
     
 }
@@ -25,3 +26,23 @@ function getFromLocalStorage() {
     var str = window.localStorage.getItem(lsKey);
     return str;
 }
+
+function draw() {
+    var canvas = document.getElementById("input_canvas");
+    var ctx = canvas.getContext("2d");
+    var text = document.getElementById('textbox').value;
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.font = "60px Amma-Regular";
+    ctx.fillText(text, 750, 190);
+    ctx.textAlign = "center";
+}
+
+function download() {
+    console.log("****");
+    var canvas = document.getElementById("input_canvas");
+    var dt = canvas.toDataURL('image/jpeg');
+    this.href = dt;
+};
+downloadLnk.addEventListener('click', download, false);
